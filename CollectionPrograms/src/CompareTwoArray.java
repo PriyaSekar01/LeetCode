dc4912a6f4838d42667ef8aeaf254722b0d52a7a
Priyasekar01 <priyasekar0137@gmail.com>
2024-08-30 17:29:41 +0530

    today updates

--- CollectionPrograms/src/CompareTwoArray.java
+++ CollectionPrograms/src/CompareTwoArray.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class CompareTwoArray {
+public class CompareTwoArray {
+
+    public static String compareArray(int[] a , int[]b){
+        if(a.length !=b.length){
+            return "Arrays is not equal";
+        }
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=b[i]){
+                return "Arrays are not equal";
+            }
+        }
+        return "Arrays are equal";
+    }
+
+
+    public static void main(String args[]) {
+        int[] a = {10, 20, 30, 40};
+        int[] b = {10, 20, 30, 40};
+        System.out.println(compareArray(a,b));
+
+    }
 }
